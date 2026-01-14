54e53cdb7607458c031bfdfdd452a5e9ff2c552d
Omar Alfarooq <alhrybymr192@gmai.com>
2026-01-14 04:17:02 +0300

    Update README and add clean student.sql with default user

---
+++ StudentsManagementSystem/Database/Database,sql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManagementSystem.Database
+{
+    internal class Database_sql
+    {
+    }
+}
